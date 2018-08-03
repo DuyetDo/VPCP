dcfa42b483dee74e9e9a19972eeb640669475833
hungDCV <hungnx@dcv.vn>
2018-08-03 15:25:32 +0700

    a

--- 03.Web_Source/SERVICE_VPCP_ADMIN/src/java/com/viettel/voffice/controller/DemoController.java
+++ 03.Web_Source/SERVICE_VPCP_ADMIN/src/java/com/viettel/voffice/controller/DemoController.java
@@ -5,6 +5,11 @@
  */
 package com.viettel.voffice.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.internal.LinkedTreeMap;
 import com.viettel.voffice.database.DAO.document.DemoDAO;
 import com.viettel.voffice.database.entity.document.Demo;
 import com.viettel.voffice.library.basecore.entity.CoreCommonEntity;
@@ -60,10 +65,13 @@ public class DemoController {
     }
     public String addOrUpdate(HttpServletRequest req, String data) {
         CoreCommonEntity commonDataClient = FunctionCommon.getRequestFromClient(
-                req, null, data, Demo.class);     
+                req, null, data, Object.class);     
         if (commonDataClient.getUserInfor() != null) {
             System.out.println(commonDataClient.getParams());
-            Demo params = (Demo) commonDataClient.getParams();
+            LinkedTreeMap sParams = (LinkedTreeMap) commonDataClient.getParams();
+            String ss = (String) sParams.get("data");
+            Gson gson = new Gson();
+            Demo params = gson.fromJson(ss, Demo.class);
             DemoDAO sysMenuDao = new DemoDAO();
             if (null == params.getId() || 0 == params.getId()) {
 //                insert
